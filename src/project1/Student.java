04c349bdc349b164feeeaef09c0dfbbf8478b214
ost96 <37448385+ost96@users.noreply.github.com>
2018-11-02 18:55:38 +0100

    Cretion of calss Student_DAO whih contains method that pushes data to dabase. Student class extendet with private fields and get/set methods. Added hibernate configuration file.

--- src/project1/Student.java
+++ src/project1/Student.java
@@ -1,4 +1,56 @@
 package project1;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
+    private int sys_num;
+
+    private String firstName;
+    private String lastname;
+    private String courseName;
+    private int semester;
+
+    public int getSys_num() {
+        return sys_num;
+    }
+
+    public void setSys_num(int sys_num) {
+        this.sys_num = sys_num;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
 }
