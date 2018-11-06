e31d8ebf3a0de7a8aee8442334c85ad70a361487
skakun <krzysztof.michalski.it>
2018-11-06 10:45:49 +0100

    init del tests with sysnums

--- src/project1/Student_DAODeleteTest.java
+++ src/project1/Student_DAODeleteTest.java
@@ -12,16 +12,16 @@ public class Student_DAODeleteTest {
         private static Student arbStudRef;
         private static void populateDatabase() {
             student_dao.setup();
-            student = new Student("Kuba", "Ostrowski", "Computer Science", 6);
+            student = new Student(1,"Kuba", "Ostrowski", "Computer Science", 6);
             student_dao.save(student);
-            student = new Student("Jacek", "Wilczyński", "Computer Science", 6);
+            student = new Student(2,"Jacek", "Wilczyński", "Computer Science", 6);
             student_dao.save(student);
             arbStudRef=student;
-            student = new Student("Paweł", "Lis", "Robotics", 4);
+            student = new Student(3,"Paweł", "Lis", "Robotics", 4);
             student_dao.save(student);
-            student = new Student("Michał", "Jakubiak", "Electrical Engineering", 7);
+            student = new Student(4,"Michał", "Jakubiak", "Electrical Engineering", 7);
             student_dao.save(student);
-            student = new Student("Konrad", "Bielecki", "Computer Science", 2);
+            student = new Student(5,"Konrad", "Bielecki", "Computer Science", 2);
             student_dao.save(student);
         }
 
